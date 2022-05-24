288b2e35c1e4761ff7b786eb6b13af5aac08ec7f
mgaidide <maksim.gaidideev@oracle.com>
2022-05-24 11:37:37 +0200

    add testdata

---
+++ src/madrat/TestData.java
@@ -0,0 +1,16 @@
+package madrat;
+
+public class TestData {
+
+    public int[] sortedIntArray = new int[] {1,2,3,4,5,6,7,8,9};
+
+    public int[] unsortedIntArray = new int[] {5,1,2,3,9,6,8,7,4};
+
+    public String testString8 = "abdsftpo";
+
+    public String testString5 = "haflp";
+
+    public String anagramString1 = "angel";
+
+    public String anagramString2 = "glean";
+}
